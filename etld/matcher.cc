81cc4f43bcaf871e1171af01bfcc7e7332379171
Peter Snyder <snyderp@gmail.com>
2019-02-08 17:30:49 -0800

    update copywrite license at top of new files 2018 -> 2019

--- etld/matcher.cc
+++ etld/matcher.cc
@@ -1,5 +1,5 @@
-/* Copyright (c) 2015 Brian R. Bondy. Distributed under the MPL2 license.
- * This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright (c) 2019 The Brave Software Team. Distributed under the MPL2
+ * license. This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
